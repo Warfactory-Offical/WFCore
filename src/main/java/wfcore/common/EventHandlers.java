c298a5598b374c7f552764665723e5cff6195438
MrNorwood <[email protected]>
2025-12-16 04:33:47 +0100

    port IDynamicModels

--- src/main/java/wfcore/common/EventHandlers.java
+++ src/main/java/wfcore/common/EventHandlers.java
@@ -4,9 +4,16 @@ package wfcore.common;
 import gregtech.api.unification.material.event.PostMaterialEvent;
 import gregtech.common.items.MetaItems;
 import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.client.event.ModelBakeEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import wfcore.api.IDynamicModels;
 import wfcore.api.material.modifications.WFCoreMaterialExtraFlags;
 import wfcore.api.material.ore.WFCoreOrePrefix;
 import wfcore.api.material.ore.WFCoreRecipeHandler;
@@ -33,5 +40,35 @@ public class EventHandlers {
 
     }
 
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onModelBake(ModelBakeEvent event) {
+        IDynamicModels.bakeModels(event);
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onPreTextureStitch(TextureStitchEvent.Pre event) {
+        IDynamicModels.registerSprites(event.getMap());
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onRegisterModels(ModelRegistryEvent event) {
+        IDynamicModels.registerModels();
+        IDynamicModels.registerCustomStateMappers();
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onItemColors(ColorHandlerEvent.Item event) {
+        IDynamicModels.registerItemColorHandlers(event);
+    }
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onBlockColors(ColorHandlerEvent.Block event) {
+        IDynamicModels.registerBlockColorHandlers(event);
+    }
 
 }
