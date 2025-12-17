e0181c3f70ee81ecce2d3cc2daae9360f83df309
MrNorwood <[email protected]>
2025-12-17 03:13:44 +0100

    clean up the event disaster

--- src/main/java/wfcore/common/items/ItemRegistry.java
+++ src/main/java/wfcore/common/items/ItemRegistry.java
@@ -5,6 +5,7 @@ import net.minecraft.item.Item;
 import java.util.HashSet;
 import java.util.Set;
 
+@SuppressWarnings("unused")
 public class ItemRegistry {
 
     //Declare public static final items here
--- src/main/java/wfcore/common/proxy/CommonProxy.java
+++ src/main/java/wfcore/common/proxy/CommonProxy.java
@@ -1,64 +1,29 @@
 package wfcore.common.proxy;
 
-import gregtech.api.unification.material.event.MaterialEvent;
-import gregtech.api.unification.material.event.PostMaterialEvent;
-import net.minecraft.block.Block;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
-import net.minecraft.item.crafting.IRecipe;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.EventPriority;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.registries.IForgeRegistry;
-import org.jetbrains.annotations.NotNull;
-import wfcore.WFCore;
-import wfcore.common.materials.WFCoreMaterials;
-import wfcore.common.metatileentities.WFCoreMetaTileEntities;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.*;
+import wfcore.common.events.RegistryEvents;
+import wfcore.common.recipe.HBMRecepies;
 
-import java.util.Objects;
-import java.util.function.Function;
-
-@Mod.EventBusSubscriber(modid = WFCore.MODID)
 public class CommonProxy {
 
-    @SubscribeEvent
-    public static void registerBlocks(@NotNull RegistryEvent.Register<Block> event) {
-        IForgeRegistry<Block> registry = event.getRegistry();
 
-        WFCoreMetaTileEntities.init();
-
-
+    public void preInit(FMLPreInitializationEvent event) {
+        MinecraftForge.EVENT_BUS.register(this);
+        MinecraftForge.EVENT_BUS.register(new RegistryEvents());
     }
 
-    @SubscribeEvent
-    public static void registerItems(@NotNull RegistryEvent.Register<Item> event) {
+    public void init(FMLInitializationEvent event) {
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public static void registerMaterials(@NotNull MaterialEvent event) {
-        WFCoreMaterials.register();
+    public void loadComplete(FMLLoadCompleteEvent event) {
+        HBMRecepies.init(event);
     }
 
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public static void postRegisterMaterials(@NotNull PostMaterialEvent event) {
-
+    public void postInit(FMLPostInitializationEvent event) {
     }
 
-    @SubscribeEvent()
-    public static void registerRecipes(RegistryEvent.Register<IRecipe> event) {
-
+    public final void serverStarting(FMLServerStartingEvent event) {
     }
 
-    private static <T extends Block> ItemBlock createItemBlock(T block, Function<T, ItemBlock> producer) {
-        ItemBlock itemBlock = producer.apply(block);
-        itemBlock.setRegistryName(Objects.requireNonNull(block.getRegistryName()));
-        return itemBlock;
-    }
-
-    public void preLoad() {
-    }
-
-    public void load() {
-    }
 }
