477a2f29f6fb6832b6932701e21562c9b3085906
MrNorwood <[email protected]>
2026-01-03 21:13:30 +0100

    update textures, update lang, fix multi structure

--- src/main/java/wfcore/client/render/WFTextures.java
+++ src/main/java/wfcore/client/render/WFTextures.java
@@ -1,17 +1,19 @@
 package wfcore.client.render;
 
 import gregtech.client.renderer.texture.cube.OrientedOverlayRenderer;
+import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class WFTextures {
 
     public static  OrientedOverlayRenderer OVERLAY_RADAR;
-
+    public static SimpleOverlayRenderer ALU_SHEET;
 
     @SideOnly(Side.CLIENT)
     public static void registerTextures(){
         OVERLAY_RADAR = new OrientedOverlayRenderer("multiblock/radar");
+        ALU_SHEET = new SimpleOverlayRenderer( "casings/wfcore_sheet_casing/aluminium_sheet_casing");
     }
 
 }
